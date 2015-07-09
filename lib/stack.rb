755e21996a8eb4166aab7ad68736d062607306e2
Shayne Clausson <sclausson@hotmail.com>
2015-07-09 17:53:56 +0200

    commit lib and spec dirs

---
+++ lib/stack.rb
@@ -0,0 +1,266 @@
+#require 'aws-sdk' #expects to load version 2
+require 'aws-sdk-v1' #version 1 for resources not yet defined in version 2
+
+#set AWS.config(region: 'region-name')
+#initialize stack with stack = AWS::CloudFormation::Stack.new('stack-name')
+
+module AWS
+  class CloudFormation
+    class Stack
+      
+      def autoscaling_group(logical_id)
+        AWS::AutoScaling.new.groups[autoscaling_group_ids[logical_id.to_sym]]
+      end
+
+      def autoscaling_groups
+        autoscaling_group_ids.inject({}) { |hash, (k,v)| hash[k] = autoscaling_group(k); hash }
+      end
+
+      def autoscaling_group_ids
+        get_resources "AWS::AutoScaling::AutoScalingGroup" 
+      end
+
+      def bucket(logical_id)
+        AWS::S3.new.buckets[bucket_ids[logical_id.to_sym]]
+      end
+
+      def buckets
+        bucket_ids.inject({}) { |hash, (k,v)| hash[k] = bucket(k); hash }
+      end
+
+      def bucket_ids
+        get_resources "AWS::S3::Bucket"
+      end
+
+      def cloudwatch_alarm(logical_id)
+        AWS::CloudWatch.new.alarms[cloudwatch_alarm_ids[logical_id.to_sym]]
+      end
+
+      def cloudwatch_alarms
+        cloudwatch_alarm_ids.inject({}) { |hash, (k,v)| hash[k] = cloudwatch_alarm(k); hash }
+      end
+
+      def cloudwatch_alarm_ids
+        get_resources "AWS::CloudWatch::Alarm"
+      end
+
+      def db_instance(logical_id)
+        AWS::RDS.new.db_instances[db_instance_ids[logical_id.to_sym]]
+      end
+
+      def db_instances
+        db_instance_ids.inject({}) { |hash, (k,v)| hash[k] = db_instance(k); hash }
+      end
+
+      def db_instance_ids
+        get_resources "AWS::RDS::DBInstance"
+      end
+
+      def eip(logical_id)
+        AWS::EC2.new.elastic_ips[eip_ids[logical_id.to_sym]]
+      end
+
+      alias_method :elastic_ip, :eip
+
+      def eips
+        eip_ids.inject({}) { |hash, (k,v)| hash[k] = eip(k) }
+      end
+
+      alias_method :elastic_ips, :eips
+
+      def eip_ids
+        get_resources "AWS::EC2::EIP"
+      end
+
+      def elb(logical_id)
+        AWS::ELB.new.load_balancers[elb_ids[logical_id.to_sym]]
+      end
+
+      def elbs
+        elb_ids.inject({}) { |hash, (k,v)| hash[k] = elb(k); hash }
+      end
+      
+      def elb_ids
+        get_resources "AWS::ElasticLoadBalancing::LoadBalancer"
+      end
+
+      def iam_access_key(user_logical_id, key_logical_id)
+        iam_user(user_logical_id).access_keys[iam_access_key_ids[key_logical_id.to_sym]]
+      end
+
+      def iam_access_key_ids
+        get_resources "AWS::IAM::AccessKey"
+      end
+
+      def iam_group(logical_id)
+        AWS::IAM.new.groups[iam_group_ids[logical_id.to_sym]]
+      end
+
+      def iam_groups
+        iam_group_ids.inject({}) { |hash, (k,v)| hash[k] = iam_group(k); hash }
+      end
+
+      def iam_group_ids
+        get_resources "AWS::IAM::Group"
+      end
+
+      def iam_policy
+       puts "use Stack.iam_group#policies or Stack.iam_user#policies"
+      end
+
+      alias_method :iam_policies, :iam_policy
+
+      def iam_policy_ids
+        get_resources "AWS::IAM::Policy"
+      end
+
+      def iam_user(logical_id)
+        AWS::IAM.new.users[iam_user_ids[logical_id.to_sym]]
+      end
+
+      def iam_users
+        iam_user_ids.inject({}) { |hash, (k,v)| hash[k] = iam_user(k); hash }
+      end
+
+      def iam_user_ids
+        get_resources "AWS::IAM::User"
+      end
+
+      def instance(logical_id)
+        AWS::EC2.new.instances[instance_ids[logical_id.to_sym]]
+      end
+
+      def instances
+        instance_ids.inject({}) { |hash, (k,v)| hash[k] = instance(k); hash }
+      end
+
+      def instance_ids
+        get_resources "AWS::EC2::Instance"
+      end
+
+      def internet_gateway(logical_id)
+        AWS::EC2.new.internet_gateways[internet_gateway_ids[logical_id.to_sym]]
+      end
+
+      alias_method :igw, :internet_gateway
+
+      def internet_gateway_ids
+        get_resources "AWS::EC2::InternetGateway"
+      end
+
+      def launch_configuration(logical_id)
+        AWS::AutoScaling.new.launch_configurations[launch_configuration_ids[logical_id.to_sym]]
+      end
+
+      alias_method :launch_config, :launch_configuration
+
+      def launch_configurations
+        launch_configuration_ids.inject({}) { |hash, (k,v)| hash[k] = launch_configuration(k); hash }
+      end
+
+      alias_method :launch_configs, :launch_configurations
+
+      def launch_configuration_ids
+        get_resources "AWS::AutoScaling::LaunchConfiguration"
+      end
+
+      def network_interface(logical_id)
+        AWS::EC2.new.network_interfaces[network_interface_ids[logical_id.to_sym]]
+      end
+
+      alias_method :nic, :network_interface
+
+      def network_interfaces
+        network_interface_ids.inject({}) { |hash, (k,v)| hash[k] = network_interface(k); hash }
+      end
+
+      alias_method :nics, :network_interfaces
+
+      def network_interface_ids
+        get_resources "AWS::EC2::NetworkInterface"
+      end
+
+      def route_table(logical_id)
+        AWS::EC2.new.route_tables[route_table_ids[logical_id.to_sym]]
+      end
+
+      def route_table_ids
+        get_resources "AWS::EC2::RouteTable"
+      end
+
+      def scaling_policy(as_group_logical_id, policy_logical_id)
+        group = autoscaling_group(as_group_logical_id)
+        policy = scaling_policy_ids[policy_logical_id.to_sym].split('/')[-1]
+        AWS::AutoScaling::ScalingPolicy.new(group, policy)
+      end
+
+      def scaling_policies(as_group_logical_id)
+        scaling_policy_ids.inject({}) { |hash, (k,v)| hash[k] = scaling_policy(as_group_logical_id,k.to_s); hash }
+      end
+
+      def scaling_policy_ids
+        get_resources "AWS::AutoScaling::ScalingPolicy"
+      end
+
+      #v1
+      def security_group(logical_id)
+        AWS::EC2.new.security_groups.filter('group-id',security_group_ids[logical_id.to_sym]).first
+      end
+
+      #v1
+      def security_groups
+        security_group_ids.inject({}) { |hash, (k,v)| hash[k] = security_group(k); hash }
+      end
+
+      def security_group_ids
+        get_resources "AWS::EC2::SecurityGroup"
+      end
+
+      def subnet(logical_id)
+        AWS::EC2.new.subnets[subnet_ids[logical_id.to_sym]]
+      end
+
+      def subnets
+        subnet_ids.inject({}) { |hash, (k,v)| hash[k] = subnet(k); hash }
+      end
+
+      def subnet_ids
+        get_resources "AWS::EC2::Subnet"
+      end
+
+      def volume(logical_id)
+        AWS::EC2.new.volumes[volume_ids[logical_id.to_sym]]
+      end
+
+      def volumes
+        volume_ids.inject({}) { |hash, (k,v)| hash[k] = volume(k); hash }
+      end
+
+      def volume_ids
+        get_resources "AWS::EC2::Volume"
+      end
+
+      def vpc(logical_id)
+        AWS::EC2.new.vpcs[vpc_ids[logical_id.to_sym]]
+      end
+
+      def vpcs
+        vpc_ids.inject({}) { |hash, (k,v)| hash[k] = vpc(k); hash }
+      end
+
+      def vpc_ids
+        get_resources "AWS::EC2::VPC"
+      end
+
+      private
+
+      def get_resources(resource_type)
+        hash = {}
+        resource_summaries.select { |rs| rs[:resource_type] == "#{resource_type}" }.collect { |r| hash[r[:logical_resource_id].to_sym] = r[:physical_resource_id] }
+        return hash
+      end
+
+    end
+  end
+end
+
---
+++ spec/stack_spec.rb
@@ -0,0 +1,245 @@
+require_relative '../lib/stack.rb'
+
+AWS.config(region: 'us-east-1')
+
+test_as = true
+test_cw = true
+test_ec2 = true
+test_elb = true
+test_iam = true
+test_rds = true
+test_s3 = true
+
+def create_test_stack(stack_name, template_file)
+  @cfn = AWS::CloudFormation.new
+  @cfn.client.create_stack({stack_name: stack_name, template_body: read(template_file), capabilities: ["CAPABILITY_IAM"]}) unless @cfn.stacks[stack_name].exists?
+  wait_for_stack(@cfn.stacks[stack_name])
+end
+
+def wait_for_stack(stack)
+  print "Waiting for stack #{stack.name} to complete" unless stack.status == "CREATE_COMPLETE"
+  until stack.status == "CREATE_COMPLETE"
+    sleep 5
+    print "."
+  end
+end
+
+def read(file)
+  File.open(file, "r").read
+end
+
+describe "AutoScaling Resources" do
+  stack_name, template_file = "AS-Test", "./spec/as-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name] 
+
+  it "#autoscaling_group('AutoScalingGroup') => Returns a valid AWS::AutoScaling::AutoScalingGroup resource" do
+    asg = stack.autoscaling_group("AutoScalingGroup")
+    expect(asg).to exist
+  end
+
+  it "#autoscaling_groups => Returns a hash of valid AWS::AutoScaling::AutoScalingGroup resources mapped to logical_ids" do
+    asgs = stack.autoscaling_groups
+    expect(asgs.values.last).to exist
+  end
+
+  it "#launch_configuration('LaunchConfig') => Returns a valid AWS::AutoScaling::LaunchConfiguration resource" do
+    lc = stack.launch_configuration("LaunchConfig")
+    expect(lc).to exist
+  end
+
+  it "#launch_configurations => Returns an array of AWS::AutoScaling::LaunchConfiguration resources" do
+    lcs = stack.launch_configurations
+    expect(lcs.values.last).to exist
+  end
+
+  it "#scaling_policy('AutoScalingGroup','ScaleUpPolicy') => Returns a valid AWS::AutoScaling::ScalingPolicy" do
+    sp = stack.scaling_policy("AutoScalingGroup","ScaleUpPolicy")
+    expect(sp).to exist
+  end
+
+  it "#scaling_policies('AutoScalingGroup') => Returns an array of valid AWS::AutoScaling::ScalingPolicy resources" do
+    sps = stack.scaling_policies("AutoScalingGroup")
+    expect(sps.values.last).to exist
+  end
+end if test_as
+
+describe "CloudWatch Resources" do
+  stack_name, template_file = "AS-Test", "./spec/as-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name]
+
+  it "#cloudwatch_alarm('CPUAlarmHigh') => Returns a valid AWS::CloudWatch::Alarm resource" do
+    cwa = stack.cloudwatch_alarm("CPUAlarmHigh")
+    expect(cwa).to exist
+  end
+
+  it "#cloudwatch_alarms => Returns a hash of valid AWS::CloudWatch::Alarm resources" do
+    cwas = stack.cloudwatch_alarms
+    expect(cwas.values.last).to exist
+  end
+end if test_cw
+
+describe "EC2 Resources" do
+  stack_name, template_file = "EC2-Test", "./spec/ec2-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name]
+
+  it "#eip('EIP') => Returns a valid AWS::EC2::EIP resource" do
+    eip = stack.elastic_ip("EIP")
+    expect(eip).to exist
+  end
+
+  it "#eips => Returns a hash of valid AWS::EC2::EIP resources" do
+    eips = stack.elastic_ips
+    expect(eips).to exist
+  end
+
+  it "#instance('Instance') => Returns a valid AWS::EC2::Instance resource" do
+    instance = stack.instance("Instance")
+    expect(instance).to exist
+  end
+
+  it "#instances => Returns hash of valid AWS::EC2::Instance resources" do
+    instances = stack.instances
+    expect(instances.values.last).to exist
+  end
+
+  it "#internet_gateway('InternetGateway') => Returns a valid AWS::EC2::InternetGateway resource" do
+    igw = stack.igw("InternetGateway")
+    expect(igw).to exist
+  end
+
+  it "#network_interface('NetworkInterface') => Returns a valid AWS::EC2::NetworkInterface resource" do
+    nic =  stack.nic("NetworkInterface")
+    expect(nic).to exist
+  end
+
+  it "#network_interfaces => Returns a hash of valid AWS::EC2::NetworkInterface resources" do
+    nics =  stack.nics
+    expect(nics.values.last).to exist
+  end
+
+  it "#route_table('RouteTable') => Returns a valid AWS::EC2::RouteTable" do
+    expect stack.route_table("RouteTable").main?
+  end
+
+  it "#security_group('SecurityGroup') => Returns a valid AWS::EC2::SecurityGroup resource" do
+    sg = stack.security_group("SecurityGroup")
+    expect(sg).to exist
+  end
+
+  it "#security_groups => Returns a hash of valid AWS::EC2::SecurityGroup resources" do
+    sgs = stack.security_groups
+    expect(sgs.values.last).to exist
+  end
+
+  it "#subnet('Subnet') => Returns a valid AWS::EC2::Subnet resource" do
+    subnet = stack.subnet("Subnet")
+    expect(subnet.state == :available)
+  end
+
+  it "#subnets => Returns a hash of valid AWS::EC2::Subnet resources" do
+    subnets = stack.subnets
+    expect(subnets.values.last.state == :available)
+  end
+
+  it "#volume('Volume') => Returns a valid AWS::EC2::Volume resource" do
+    volume = stack.volume('Volume')
+    expect(volume).to exist
+  end
+
+  it "#volumes => Returns a hash of valid AWS::EC2::Volume resources" do
+    volumes = stack.volumes
+    expect(volumes.values.last).to exist
+  end
+
+  it "#vpc('VPC') => Returns a valid AWS::EC2::VPC resource" do
+    vpc = stack.vpc('VPC')
+    expect(vpc).to exist
+  end
+
+  it "#vpcs => Returns a valid AWS::EC2::VPC resource" do
+    vpcs = stack.vpcs
+    expect(vpcs.values.last).to exist
+  end
+end if test_ec2
+
+describe "ELB Resources" do
+  stack_name, template_file = "AS-Test", "./spec/as-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name]
+
+  it "#elb('ElasticLoadBalancer') => Returns a vaild AWS::ElasticLoadBalancing::LoadBalancer resource" do
+    elb = stack.elb("ElasticLoadBalancer")
+    expect(elb).to exist
+  end
+
+  it "#elbs => Returns a hash of vaild AWS::ElasticLoadBalancing::LoadBalancer resource" do
+    elbs = stack.elbs
+    expect(elbs.values.last).to exist
+  end
+end if test_elb
+
+describe "IAM Resources" do
+  stack_name, template_file = "IAM-Test", "./spec/iam-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name]
+
+  it "#iam_access_key('IAMUser','IAMAccessKey') => Returns a valid AWS::IAM::AccessKey resource" do
+    ak = stack.iam_access_key("IAMUser","IAMAccessKey")
+    expect(ak.status).to eq :inactive
+  end 
+
+  it "#iam_group('IAMGroup') => Returns a valid AWS::IAM::Group resource" do
+    group = stack.iam_group("IAMGroup")
+    expect(group).to exist
+  end
+
+  it "#iam_groups => Returns a hash of valid AWS::IAM::Group resources" do
+    groups = stack.iam_groups
+    expect(groups.values.last).to exist
+  end
+
+  it "#iam_user('IAMUser') => Returns a valid AWS::IAM::User resource" do
+    user = stack.iam_user("IAMUser")
+    expect(user).to exist
+  end
+  
+  it "#iam_users => Returns a hash of valid AWS::IAM::User resources" do
+    users = stack.iam_users
+    expect(users.values.last).to exist
+  end
+end if test_iam
+
+describe "RDS Resources" do
+  stack_name, template_file = "RDS-Test", "./spec/rds-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name] 
+
+  it "#db_instance('myDB') => Returns a valid AWS::RDS::DBInstance resource" do
+    dbi = stack.db_instance("myDB")
+    expect(dbi).to exist
+  end
+
+  it "#db_instances => Returns a hash of valid AWS::RDS::DBInstance resources" do
+    dbis = stack.db_instances
+    expect(dbis.values.last).to exist
+  end 
+end if test_rds
+
+describe "S3 Resources" do
+  stack_name, template_file = "S3-Test", "./spec/s3-test.template"
+  create_test_stack(stack_name,template_file)
+  stack = @cfn.stacks[stack_name]
+
+  it "#bucket('S3Bucket') => Returns a valid AWS::S3::Bucket resource" do
+    bucket = stack.bucket("S3Bucket")
+    expect(bucket).to exist
+  end
+
+  it "#buckets => Returns a hash of valid AWS::S3::Bucket resources" do
+    buckets = stack.buckets
+    expect(buckets.values.last).to exist
+  end
+end if test_s3
